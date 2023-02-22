8017477d6680cdcb1326147abceb88588c7c380a
eriko20 <109963382+eriko20@users.noreply.github.com>
2023-02-22 17:50:47 +0700

    lumen
    Api

---
+++ api-lumen/database/seeders/KategoriSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kategori;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i=0; $i < 100; $i++) { 
+            $data = [
+                'kategori' => $faker->name,
+                'keterangan' => $faker->text
+            ];
+            Kategori::create($data);
+        }
+       
+    }
+}
